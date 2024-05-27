dc60b011bc4deaa486a91a5d2733cc166105f13b
Francisco J. Casillas <chiccasillas@gmail.com>
2024-05-27 12:11:05 -0600

    Define ProductCard component

---
+++ src/components/ProductCard.jsx
@@ -0,0 +1,17 @@
+import PropTypes from "prop-types";
+
+function ProductCard({ name, price = 0, inStock = true }) {
+	return (
+		<div>
+			<h2>{name}</h2>
+			<p>Price: ${price.toFixed(2)}</p>
+			{inStock ? <button>Add to cart</button> : <p>Out of stock</p>}
+		</div>
+	);
+}
+
+ProductCard.propTypes = {
+	name: PropTypes.string.isRequired,
+	price: PropTypes.number,
+	inStock: PropTypes.bool,
+};
